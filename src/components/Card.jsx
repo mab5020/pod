0a1b75eeb7f8ed44ecd9ed58daf8747696fde512
mab5020 <abhimane0303@gmail.com>
2025-05-28 18:38:56 +0530

    Added dark mode

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 function Card(props){
-    return <div className="w-48 overflow-hidden shadow-lg bg-white border border-gray-200 rounded-lg">
+    return <div className={`w-48 overflow-hidden shadow-lg ${props.theme ? " bg-stone-800 border border-stone-800" :  "bg-white border border-gray-400"} rounded-lg`}>
         <img className="w-full object-cover" width={100} height={100} src={props.img} alt="Card Image" />
-        <div className="">
-            <h1 className="ml-2 text-lg font-semibold text-gray-800">{props.title.length > 12 ? `${props.title.slice(0,9)}...`: props.title}</h1>
-            <p className="text-gray-600 ml-2">{props.description.length > 19 ? `${props.description.slice(0,16)}...`: props.description}</p>
-            <button className="bg-blue-500 cursor-pointer text-white px-4 py-2 rounded-lg m-2 hover:bg-blue-600 transition"><a href={props.link}>Listen...</a></button>
+        <div className={`${props.theme ? "text-white" : "text-black"}`}>
+            <h1 className="ml-2 text-lg font-semibold">{props.title.length > 12 ? `${props.title.slice(0,9)}...`: props.title}</h1>
+            <p className="ml-2">{props.description.length > 19 ? `${props.description.slice(0,16)}...`: props.description}</p>
+            <button className="bg-green-500 cursor-pointer text-white px-4 py-2 rounded-lg m-2 hover:bg-blue-600 transition"><a href={props.link}>Listen...</a></button>
         </div>
     </div>
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
 import axios from "axios";
+import sun from "../assets/sunny.png";
+import moon from "../assets/moon.png";
 
 function Home() {
+    const [dark, setDark] = useState(false);
     const [data, setData] = useState(null);
     const [input, setInput] = useState("");
     const [token, setToken] = useState(null);
@@ -17,16 +20,16 @@ function Home() {
                     "Authorization": 'Basic ' + btoa(clientID + ':' + clientSecret)
                 }
             })
-            console.log(response.data);
+            // console.log(response.data);
 
-            console.log(response.data.access_token);
+            // console.log(response.data.access_token);
             setToken(response.data.access_token);
 
         } catch (error) {
             console.log("error: ", error);
         }
     }
-    function handleEnter(e){
+    function handleEnter(e) {
         if (e.key === "Enter") {
             searchPod(e);
         }
@@ -57,11 +60,14 @@ function Home() {
         fetchToken();
     }, [])
 
-    return <div className="home-container flex flex-col items-center">
+    return <div className={`home-container flex flex-col items-center ${dark ? "bg-stone-600" : null}`}>
+        <div className="theme-btn fixed top-2 right-2 m-2 rounded-full">
+            <button className={`p-2 rounded-full bg-green-600`} onClick={() => setDark(!dark)}>{dark ? <img height="32px" width="32px" src={sun} alt="sun"/> : <img height="32px" width="32px" src={moon} alt="sun"/>}</button>
+        </div>
         <div className="w-3/5 flex flex-col justify-center items-center mt-[10%]">
-            <h1 className="text-[40px] font-semibold"><span className="text-green-600">Pod</span><span>Fans</span></h1>
+            <h1 className="text-[40px] font-semibold"><span className="text-green-600">Pod</span><span className={`${dark ? "text-white" : null}`}>Fans</span></h1>
             <div className="w-3/5 flex flex-inline rounded-full border-2 border-green-600">
-                <input onKeyUp={(e)=>handleEnter(e)} className="px-2 py-2 w-full outline-none" onChange={(e) => setInput(e.target.value)} value={input} placeholder="What's on your mind?" type="text" /><button className="px-2 py-2 cursor-pointer" onClick={(e) => searchPod(e)}><i className="fa fa-search" aria-hidden="true"></i></button>
+                <input onKeyUp={(e) => handleEnter(e)} className={`px-2 py-2 text-[20px] w-full outline-none ${dark ? "text-white" : null}`} onChange={(e) => setInput(e.target.value)} value={input} placeholder="What's on your mind?" type="text" /><button className={`px-2 py-2 cursor-pointer ${dark ? "text-white" : null}`} onClick={(e) => searchPod(e)}><i className="fa fa-search" aria-hidden="true"></i></button>
             </div>
         </div>
         {
@@ -69,9 +75,9 @@ function Home() {
                 <h2 className="text-[30px] font-semibold text-gray-600">Recommendations...</h2>
                 <div className="flex flex-row overflow-auto">
                     {
-                        data.map((item, index)=>{
+                        data.map((item, index) => {
                             return <div key={index} className="m-2">
-                                <Card key={index} img={item.images[0].url} title={item.name} description={item.publisher} link={item.external_urls.spotify} />
+                                <Card theme={dark} key={index} img={item.images[0].url} title={item.name} description={item.publisher} link={item.external_urls.spotify} />
                             </div>
                         })
                     }
